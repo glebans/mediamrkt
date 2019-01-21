c3fd3faf25142f6d2061608ea18955f8fc92eb8c
glebans <glebansby@gmail.com>
2019-01-21 18:58:34 +0300

    gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,47 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+// Static Server
+gulp.task('serve', () => {
 
     browserSync.init({
-        server: "./"
+        server: "./dist/"
     });
 
-    gulp.watch("./scss/*.scss", ['sass']);
+    gulp.watch("./scss/*.scss", gulp.parallel('sass'));
+    gulp.watch("*.html", gulp.parallel('html'));
+    gulp.watch("*./font/**/*.*", gulp.parallel('font'));
+    gulp.watch("*./img/**/*", gulp.parallel('img'));
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
+// sass
+gulp.task('sass', () => {
     return gulp.src("./scss/*.scss")
         .pipe(sass())
-        .pipe(gulp.dest("./css"))
+        .pipe(gulp.dest("./dist/css/"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['serve']);
+gulp.task('html', () => {
+    return gulp.src("./*.html")
+        .pipe(gulp.dest("./dist/"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('img', () => {
+    return gulp.src("*./img/**/*")
+        .pipe(gulp.dest("./dist/img"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('font', () => {
+    return gulp.src("*./font/**/*")
+        .pipe(gulp.dest("./dist/font"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', gulp.parallel('serve', 'sass', 'html', 'img', 'font'));
 
  
